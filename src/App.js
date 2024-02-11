79b381ba08f3a4a2e1ea96fad6e55669d4c6271b
uqreative <syukri.gazali.mohammad@gmail.com>
2024-02-11 15:02:44 +0700

    insert navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import KonekDong from './pages/cobaConnect';
 import YangKedua from './pages/kedua';
-import HeaderNav from './pages/navbar';
+import HeaderNav from './pages/inc-template/navbar';
+import Footer from './pages/inc-template/footer';
 
 
 function App() {
   return (
     <div>
+      <HeaderNav></HeaderNav>
       <h1>Masa gk nyambung sih ?</h1>
       <h1 className="text-3xl font-bold underline">
       Hello world!ddd
-    </h1>
+      </h1>
       <KonekDong></KonekDong>
       <YangKedua></YangKedua>
-      <HeaderNav></HeaderNav>
+      <Footer></Footer>
     </div>
   );
 }

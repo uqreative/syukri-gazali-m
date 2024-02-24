3c955c07df87116cc0aef7bbeabdb321ee0a8b2e
uqreative <syukri.gazali.mohammad@gmail.com>
2024-02-24 16:05:01 +0700

    Update navbar.js

--- src/pages/inc-template/navbar.js
+++ src/pages/inc-template/navbar.js
@@ -1,36 +1,45 @@
 export default function HeaderNav (){
     return (
-      <div className="flex items-center justify-around bg-blue-700 px-5 py-4">
-        <div className="flex items-center justify-between">
-          <div className="text-nowrap text-2xl capitalize font-bold text-white">Syukri Gazali</div>
-          <div className="block lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-              />
-            </svg>
-          </div>
-        </div>
-        <div className="flex items-center justify-between w-full ">
-            <div className="flex gap-x-5 items-center justify-center w-full">
-                <a href="#" className="block px-3 py-4 text-white hover:text-blue-200">My Story</a>
-                <a href="#" className="block px-3 py-4 text-white hover:text-blue-200">Ability & Skills</a>
-                <a href="#" className="block px-3 py-4 text-white hover:text-blue-200">My Crafted Work</a>
-                <a href="#" className="block px-3 py-4 text-white hover:text-blue-200">Contact</a>
-            </div>
+      <div className="bg-gradient-to-br from-blue-700 to-pink-500">
+        <div className="flex flex-col lg:flex-row">
+
+          <div className="flex items-center justify-between px-4 py-4 lg:py-0 border-b  border-blue-700 lg:border-b-0">
+            <div className="text-nowrap text-2xl capitalize font-bold text-white">Syukri Gazali</div>
             <div>
-                <a href="#" className="block px-3 py-2 text white text-nowrap bg-white hover:bg-blue-500">Button CTA</a>
+              <button className="focus:outline-none text-white block lg:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                </svg>
+              </button>
             </div>
+          </div>
+
+          <div className="flex flex-col lg:flex-row justify-between w-full py-4 lg:py-0">
+            <div className="flex flex-col lg:flex-row gap-x-5 items-start lg:items-center justify-clLenter">
+              <a href="#" className="block px-3 py-4 text-white hover:text-blue-200">My Story</a>
+              <a href="#" className="block px-3 py-4 text-white hover:text-blue-200">Ability & Skills</a>
+              <a href="#" className="block px-3 py-4 text-white hover:text-blue-200">My Crafted Work</a>
+              <a href="#" className="block px-3 py-4 text-white hover:text-blue-200">Contact</a>
+            </div>
+            <div className="flex flex-col lg:flex-row gap-x-5">
+              <a href="#" className="block m-2 px-3 py-2 text white text-nowrap bg-white hover:bg-blue-500">Button CTA</a>
+              <a href="#" className="block m-2 px-3 py-2 text white text-nowrap bg-white hover:bg-blue-500">Button Login</a>
+            </div>
+          </div>
+
         </div>
       </div>
+      
     );
 };

c81be24a46a2af4fc82a9bff452b7b93f7f8daa3
uqreative <syukri.gazali.mohammad@gmail.com>
2024-04-12 20:48:43 +0700

    Add swiper Hero

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import KonekDong from './pages/cobaConnect';
-import YangKedua from './pages/kedua';
-import HeaderNav from './pages/inc-template/navbar';
-import Footer from './pages/inc-template/footer';
-import HeroBig from './pages/component/hero';
-
+import React from "react";
+import KonekDong from "./pages/cobaConnect";
+import YangKedua from "./pages/kedua";
+import HeaderNav from "./pages/inc-template/navbar";
+import Footer from "./pages/inc-template/footer";
+// import HeroBig from './pages/component/hero';
+import SwiperHero from "./pages/component/hero";
 
 function App() {
   return (
     <div>
       <HeaderNav></HeaderNav>
-      <HeroBig></HeroBig>
+      {/* <HeroBig></HeroBig> */}
+      <SwiperHero></SwiperHero>
       <h1>Masa gk nyambung sih ?</h1>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!ddd
-      </h1>
+      <h1 className="text-3xl font-bold underline">Hello world!ddd</h1>
       <KonekDong></KonekDong>
       <YangKedua></YangKedua>
       <Footer></Footer>
--- src/pages/component/hero.jsx
+++ src/pages/component/hero.jsx
@@ -1,7 +1,49 @@
-export default function HeroBig (){
-    return (
-      <div className="w-full h-hero bg-slate-200">
-        <h1 className="text-5xl">Hero Main</h1>
-      </div>
-    );
-};
+// export default function HeroBig (){
+//     return (
+//       <div className="w-full h-hero bg-slate-200">
+//         <h1 className="text-5xl">Hero Main</h1>
+//       </div>
+//     );
+// };
+
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+import "./hero.css";
+
+// import required modules
+import { Pagination } from "swiper/modules";
+
+export default function SwiperHero() {
+  return (
+    <>
+      <Swiper pagination={true} modules={[Pagination]} className="mySwiper ">
+        <SwiperSlide>
+          <div className="bg-rose-500">
+            <div className="px-16 ">
+              <h2 className="text-3xl font-black">Slide 1</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
+                perferendis ullam sequi suscipit obcaecati doloribus recusandae
+                error eveniet ipsam provident amet labore sunt itaque corporis,
+                deleniti saepe dicta soluta laboriosam magnam unde vitae dolorem
+                quasi hic aliquid. Beatae fuga maiores provident, alias
+                reiciendis expedita? Quisquam, eius tempore? Quasi, aperiam
+                deleniti.
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
